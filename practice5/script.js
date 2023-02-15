7b343a8b20c61bdeceaa38592f87388dd834b7a1
Gulsevar <117287145+sugdiyonaRaxmatullayeva@users.noreply.github.com>
2023-02-15 01:26:15 -0800

    Add files via upload

---
+++ practice5/script.js
@@ -0,0 +1,392 @@
+"use stript"
+//SIWICH
+
+//1 masala
+// let nu mber = Number(prompt("a son"));
+
+// let output;
+
+// switch (number) {
+//   case 1:
+//     output = "dushanba";
+//     break;
+//   case 2:
+//     output="seshanba" ;
+//     break;
+//   case 3:
+//     output="chorshanba" ;
+//     break;
+//  case 4:
+//      output="payshanba";
+//   break;
+//   case 5:
+//     output="juma";
+//    break;
+//    case 6:
+//     output="shanba" ;
+//     break;
+//     case 7:
+//         output="yakshanba"
+//         default:
+//             output="topilmadi"
+// }
+
+// console.log(output);
+
+//2masala
+// let number = Number(prompt("k son"));
+
+// let output;
+
+// switch (number) {
+//   case 1:
+//     output = "yomon";
+//     break;
+//   case 2:
+//     output="qoniqarsiz" ;
+//     break;
+//   case 3:
+//     output="qoniqarli" ;
+//     break;
+//  case 4:
+//      output="yahshi";
+//   break;
+//   case 5:
+//     output="a'lo";
+//    break;
+   
+//         default:
+//             output="xato"
+// }
+
+// console.log(output);
+
+//3 masala
+// let number = Number(prompt("a son"));
+
+// let output;
+
+// switch (number) {
+//   case 12:
+//     output = "qish";
+//     break;
+//     case 1:
+//     output = "qish";
+//     break;
+//     case 2:
+//     output = "qish";
+//     break;
+//   case 3:
+//     output="bahor" ;
+//     break;
+//     case 4:
+//     output="bahor" ;
+//     break;
+//     case 5:
+//     output="bahor" ;
+//     break;
+//   case 6:
+//     output="yoz" ;
+//     break;
+//     case 7:
+//     output="yoz" ;
+//     break;
+//     case 8:
+//     output="yoz" ;
+//     break;
+//  case 9:
+//      output="kuz";
+//      break;
+//      case 10:
+//      output="kuz";
+//      break;
+//      case 11:
+//      output="kuz";
+//      break;
+//         default:
+//             output="topilmadi"
+// }
+
+// console.log(output);
+
+//4 masala
+// let number = Number(prompt("a son"));
+
+// let output;
+
+// switch (number) {
+//   case 12:
+//     output = "dekabr 31kun";
+//     break;
+//     case 1:
+//     output = "yanvar 31kun";
+//     break;
+//     case 2:
+//     output = "fevral 28kun";
+//     break;
+//   case 3:
+//     output="mart 31kun" ;
+//     break;
+//     case 4:
+//     output="aprel 30 kun" ;
+//     break;
+//     case 5:
+//     output="may 31 kun" ;
+//     break;
+//   case 6:
+//     output="iyun 30kun" ;
+//     break;
+//     case 7:
+//     output="iyul 31 kun" ;
+//     break;
+//     case 8:
+//     output="avgust 31kun" ;
+//     break;
+//  case 9:
+//      output="sentabr 30 kun";
+//      break;
+//      case 10:
+//      output="oktyabr 31 kun";
+//      break;
+//      case 11:
+//      output="noyabr 30 kun";
+//      break;
+//         default:
+//             output="topilmadi"
+// }
+
+// console.log(output);
+
+//5 masala
+
+// let a = Number(prompt("a son"));
+// let b=Number(prompt("b son"));
+// let javob=Number(prompt("qaysi operator= 1 , 2, 3 , 4 "));
+
+// let output;
+
+// switch (javob) {
+  
+//     case 1:
+//     output = `${a+b}`;
+//     break;
+//     case 2:
+//     output = `${a-b}`;
+//     break;
+//   case 3:
+//     output=`${a/b}` ;
+//     break; 
+//     case 4:
+//     output=`${a*b}` ;
+//     break;
+    
+//         default:
+//             output="topilmadi"
+// }
+
+// console.log(output);
+
+//6 masala
+// let son=Number(prompt("haqiqiy son"));
+// let birlik =Number(prompt("birlik tanlang=1-dm, 2-km ,3-m ,4-mm ,5 sm"));
+
+// let output;
+
+// switch(birlik){
+//     case 1:
+//     output=`${son*0.1}"metr"`;
+//     break;
+//     case 2:
+//         output=`${son*1000}"metr"`;
+//     break;
+//     case 3:
+//         output=`${son*1}"metr"`;
+//         break;
+//       case 4:
+//        output=`${son*0.001}"metr"` ;
+//        break;
+//        case 5:
+//         output=`${son*0,01}"metr"` ;
+//         break;
+//         default:
+//             output="bunday son kiritilmagan";
+
+// }
+
+// console.log(output);
+
+
+//7 masala 
+// let son=Number(prompt("haqiqiy son"));
+// let birlik =Number(prompt(" ogirlik birliklaridan tanlang=1-kg, 2-mg, 3-gr ,4-tonna ,5 sentiner"));
+
+// let output;
+
+// switch(birlik){
+//     case 1:
+//     output=`${son*1}"kg"`;
+//     break;
+//     case 2:
+//         output=`${son/10**6}"kg"`;
+//     break;
+//     case 3:
+//         output=`${son/1000}"kg"`;
+//         break;
+//       case 4:
+//        output=`${son*1000}"kg"` ;
+//        break;
+//        case 5:
+//         output=`${son*100}"kg"` ;
+//         break;
+//         default:
+//             output="bunday son kiritilmagan";
+
+// }
+
+// console.log(output);
+
+//8 masala 
+// let sana = Number(prompt("sana"));
+// let oy =Number(prompt("oy"))
+// let output;
+
+// switch (oy) {
+//   case 12:
+//     output = `${sana}-dekabr`;
+//     break;
+//     case 1:
+//     output = `${sana}-yanvar`;
+//     break;
+//     case 2:
+//     output = `${sana}-fevral`;
+//     break;
+//   case 3:
+//     output=`${sana}-mart` ;
+//     break;
+//     case 4:
+//     output=`${sana}-aprel`  ;
+//     break;
+//     case 5:
+//     output=`${sana}-may ` ;
+//     break;
+//   case 6:
+//     output=`${sana}-iyun ` ;
+//     break;
+//     case 7:
+//     output=`${sana}-iyul`  ;
+//     break;
+//     case 8:
+//     output=`${sana}-avgust` ;
+//     break;
+//  case 9:
+//      output=`${sana}-sentabr`;
+//      break;
+//      case 10:
+//      output=`${sana}-oktyabr`;
+//      break;
+//      case 11:
+//      output=`${sana}-noyabr`;
+//      break;
+//         default:
+//             output="topilmadi"
+// }
+
+// console.log(output);
+
+//9 masala
+// let sana = Number(prompt("sana"));
+// let oy =Number(prompt("oy"))
+// let output;
+
+// switch (oy) {
+//   case 12:
+//     output = `${sana +1}-dekabr`;
+//     break;
+//     case 1:
+//     output = `${sana+1}-yanvar`;
+//     break;
+//     case 2:
+//     output = `${sana+1}-fevral`;
+//     break;
+//   case 3:
+//     output=`${sana+1}-mart` ;
+//     break;
+//     case 4:
+//     output=`${sana+1}-aprel`  ;
+//     break;
+//     case 5:
+//     output=`${sana+1}-may ` ;
+//     break;
+//   case 6:
+//     output=`${sana+1}-iyun ` ;
+//     break;
+//     case 7:
+//     output=`${sana+1}-iyul`  ;
+//     break;
+//     case 8:
+//     output=`${sana+1}-avgust` ;
+//     break;
+//  case 9:
+//      output=`${sana+1}-sentabr`;
+//      break;
+//      case 10:
+//      output=`${sana+1}-oktyabr`;
+//      break;
+//      case 11:
+//      output=`${sana+1}-noyabr`;
+//      break;
+//         default:
+//             output="topilmadi"
+// }
+
+// console.log(output);
+
+// 10 masala
+let birlik =(prompt("yonalishni tanlang s=shimol,j=janub,q=sharq,g=g'arb"));
+let kamanda=Number(prompt("kamanda tanlang =0-harakatni davom etirv ,1- o'ngga burul,2-chapga burul"))
+let javob=String(`${birlik}${kamanda}`)
+let output;
+
+switch(javob){
+    case "s2":
+    output=`sharqda`;
+    break;
+    case "s1":
+        output=`g'arb`;
+    break;
+    case "s0":
+        output=`janub`;
+        break;
+      case "j1":
+       output=`sharq` ;
+       break;
+       case "j2":
+        output=`g'arb` ;
+        break;
+        case "j0":
+        output=`shimol` ;
+        break;
+        case "q1":
+        output=`shimol` ;
+        break;
+        case "q2":
+        output=`janub` ;
+        break;
+        case "q0":
+        output=`g'arb` ;
+        break;
+        case "g1":
+        output=`janub` ;
+        break;
+        case "g2":
+        output=`shimol` ;
+        break;
+        case "g0":
+        output=`sharq` ;
+        break;
+        default:
+            output="bunday son kiritilmagan";
+
+}
+
+console.log(output);
